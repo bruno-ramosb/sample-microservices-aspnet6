812e1479bfc7d6fdc67b07c22eb8952c101341ee
Bruno Ramos <brunoramos.bsilva@gmail.com>
2022-05-24 07:54:06 -0300

    adicionando entidade de produtos

---
+++ GeekShopping.ProductApi/Model/Context/PgsqlContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductApi.Model.Context
+{
+    public class PgsqlContext: DbContext
+    {
+        public PgsqlContext()
+        {
+        }
+        public PgsqlContext(DbContextOptions<PgsqlContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+       
+    }
+}
