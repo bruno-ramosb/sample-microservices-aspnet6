a0b7f0ae2b6d0ce4a52a0ce8223689097117d5ab
Bruno Ramos <brunoramos.bsilva@gmail.com>
2022-05-31 13:47:26 -0300

    adicionando dbcontext identity

---
+++ GeekShopping.IdentityServer/Model/Context/PgsqlContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Models.Model.Context
+{
+    public class PgsqlContext : IdentityDbContext<ApplicationUser>
+    {
+        public PgsqlContext(DbContextOptions<PgsqlContext> options) : base(options)
+        {
+        }
+    }
+}
